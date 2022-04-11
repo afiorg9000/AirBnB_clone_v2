efca36bf0a7a92c8b7facdb27919b7fe8946fbe2
afiorg9000 <3533@holbertonschool.com>
2022-04-11 20:51:35 +0000

    all instance add

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,13 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        obj_of_class = {}
+        if cls is None:
+            return FileStorage.__objects
+        for key, value in FileStorage.__objects.items():
+            if value.__class__ == cls:
+                obj_of_class[key] = value
+        return obj_of_class
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
