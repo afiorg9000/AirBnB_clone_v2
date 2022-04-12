dae98ec22aa78d619870d6500d0c896b6da8645f
WeatherBoyGPR <gp.rossijr@gmail.com>
2022-04-12 16:52:23 +0000

    updated state class to have relationship with city class

--- models/state.py
+++ models/state.py
@@ -2,9 +2,21 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 class State(BaseModel, base):
     """ State class """
     __tablename__ = "states"
 
-    name = Column(String(128), nullable=False
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', cascade="all, delete")
+    
+
+    @property
+    def cities(self):
+        """ Will return list of cities with state_id """
+        res = []
+        for city in self.cities:
+            if city.state_id == self.id:
+                res.append(city)
+        return res
