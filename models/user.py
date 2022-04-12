ac77e04d8c0b02fc092e5e92bb27873dd6fa75a9
WeatherBoyGPR <gp.rossijr@gmail.com>
2022-04-12 17:47:39 +0000

    fixed minor typo

--- models/user.py
+++ models/user.py
@@ -2,7 +2,7 @@
 """This module defines a class User"""
 from models.base_model import BaseModel
 from sqlalchemy import Column, String
-from aqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
