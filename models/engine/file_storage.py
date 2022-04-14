0ccd4c655f331262425f8a2849a9216fce62008f
WeatherBoyGPR <gp.rossijr@gmail.com>
2022-04-14 19:13:27 +0000

    updated file_storage to use self.all in reload method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -51,7 +51,7 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                    self.__objects[key] = classes[temp[val]['__class__']](**temp[key])
+                    self.all()[key] = classes[val['__class__']](**val)
         except:
             pass
 
