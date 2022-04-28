9b4e53941fb42fd2cc8fa56a03d593b0f20cdb5f
afiorg9000 <3533@holbertonschool.com>
2022-04-28 20:11:38 +0000

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,7 +6,6 @@ sudo mkdir /data/web_static/
 sudo mkdir /data/web_static/releases/
 sudo mkdir /data/web_static/shared/
 sudo mkdir /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
 echo '<html>
   <head>
   </head>
@@ -14,8 +13,8 @@ echo '<html>
     Holberton School
   </body>
 </html>' > /data/web_static/releases/test/index.html
-sudo rm -rf /data/web_static/current
 sudo ln -s /data/web_static/current /data/web_static/releases/test/
-sudo chown -hR ubuntu:ubuntu /data/
-sudo cp -r /data/web_static/current/* /data/web_static/$1
+sudo chown -R ubuntu:ubuntu /data/
+sed -i "58i \\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
 sudo service nginx restart
+exit 0
